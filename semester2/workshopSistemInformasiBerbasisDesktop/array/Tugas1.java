60607c9484b965adc781163f54e5b1a1d905bb07
Achmad Maulana Ali Hamzah <140522454+ITMaulanaali@users.noreply.github.com>
2025-03-10 01:08:20 +0700

    Mengiterasi sederhana full name di array dengan for each

---
+++ semester2/workshopSistemInformasiBerbasisDesktop/array/Tugas1.java
@@ -0,0 +1,10 @@
+class testing{
+    static String[] nama = {"Achmad","Maulana","Ali","Hamzah"};
+    
+    public static void main(String[] args){
+        
+        for(String i : nama){
+            System.out.print(i + " ");
+        }
+    }
+}
